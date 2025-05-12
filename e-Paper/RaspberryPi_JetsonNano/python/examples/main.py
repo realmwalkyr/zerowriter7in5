ca573505da9bd4083273047fcd0ab2e94935d45e
realmwalkyr <88550926+realmwalkyr@users.noreply.github.com>
2025-05-11 20:35:22 -0500

    Update main.py driver
    epd7in5_V2

--- e-Paper/RaspberryPi_JetsonNano/python/examples/main.py
+++ e-Paper/RaspberryPi_JetsonNano/python/examples/main.py
@@ -12,14 +12,14 @@
 # access to the SPI screen. it handles keyboard input directly via keyboard library.
 #
 # currently ONLY supports waveshare 4in2
-# Modifications to change support to the Waveshare 7.5 inch 800x480 display - new7in5part_zw
+# Modifications to change support to the Waveshare 7.5 inch 800x480 display - epd7in5_V2
 
 import time
 import keyboard
 import keymaps
 from PIL import Image, ImageDraw, ImageFont
 #from waveshare_epd import epd4in2_V2
-from waveshare_epd import new7in5part_zw
+from waveshare_epd import epd7in5_V2
 from zerowriter import ZeroWriter
 
 
@@ -27,7 +27,7 @@ from zerowriter import ZeroWriter
 zero_writer = ZeroWriter()
 
 try:
-  zero_writer.epd = new7in5part_zw.EPD()
+  zero_writer.epd = epd7in5_V2.EPD()
   zero_writer.keyboard = keyboard
   zero_writer.initialize()
   zero_writer.run()
