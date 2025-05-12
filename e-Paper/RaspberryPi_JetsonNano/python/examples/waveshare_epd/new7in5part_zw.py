cf63817eccabcf62234da97029fb1f4f9b177337
realmwalkyr <88550926+realmwalkyr@users.noreply.github.com>
2025-05-11 20:03:08 -0500

    Update new7in5part_zw.py

--- e-Paper/RaspberryPi_JetsonNano/python/examples/waveshare_epd/new7in5part_zw.py
+++ e-Paper/RaspberryPi_JetsonNano/python/examples/waveshare_epd/new7in5part_zw.py
@@ -320,7 +320,7 @@ class EPD:
         epdconfig.delay_ms(100)
         self.ReadBusy()
 
-    def display_Partial(self, Image):
+    def display_Partial(self, image):
         if self.width % 8 == 0:
             linewidth = int(self.width / 8)
         else:
