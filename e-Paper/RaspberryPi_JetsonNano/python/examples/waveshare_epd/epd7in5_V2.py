df2d67c4ef1f8c86458793e93b05ee4eaf80bec8
realmwalkyr <88550926+realmwalkyr@users.noreply.github.com>
2025-05-15 10:41:09 -0500

    Update epd7in5_V2.py

--- e-Paper/RaspberryPi_JetsonNano/python/examples/waveshare_epd/epd7in5_V2.py
+++ e-Paper/RaspberryPi_JetsonNano/python/examples/waveshare_epd/epd7in5_V2.py
@@ -340,8 +340,8 @@ class EPD:
 #        Height = Yend - Ystart
 	
         self.send_command(0x50)
-        #self.send_data(0xA9)
-        self.send_data(0x07)
+        self.send_data(0xA9)
+        #self.send_data(0x07)
 
         self.send_command(0x92)		#This command makes the display enter partial mode
         self.send_command(0x90)		#resolution setting
