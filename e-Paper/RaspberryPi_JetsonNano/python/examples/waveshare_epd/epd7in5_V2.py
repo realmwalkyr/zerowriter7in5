e37ec87d423b701d36bc33329f15df3a82a2b2b4
realmwalkyr <88550926+realmwalkyr@users.noreply.github.com>
2025-05-15 12:42:53 -0500

    Update epd7in5_V2.py

--- e-Paper/RaspberryPi_JetsonNano/python/examples/waveshare_epd/epd7in5_V2.py
+++ e-Paper/RaspberryPi_JetsonNano/python/examples/waveshare_epd/epd7in5_V2.py
@@ -363,7 +363,7 @@ class EPD:
                 for i in range(Width):
                     image1[i + j * Width] = ~Image[i + j * Width]
         
-        self.send_command(0x10)
+        #self.send_command(0x10)
         self.send_command(0x13)   #Write Black and White image to RAM
         self.send_data2(image1)
 
