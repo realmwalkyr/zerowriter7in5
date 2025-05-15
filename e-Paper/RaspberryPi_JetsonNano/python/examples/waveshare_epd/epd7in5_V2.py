95456ece8126fc3bbad1cc412444964002f0b169
realmwalkyr <88550926+realmwalkyr@users.noreply.github.com>
2025-05-15 10:17:54 -0500

    Update epd7in5_V2.py

--- e-Paper/RaspberryPi_JetsonNano/python/examples/waveshare_epd/epd7in5_V2.py
+++ e-Paper/RaspberryPi_JetsonNano/python/examples/waveshare_epd/epd7in5_V2.py
@@ -356,7 +356,7 @@ class EPD:
 
 #        self.send_data ((Yend-1)//256)		
 #        self.send_data ((Yend-1)%256)  #y-end
-        self.send_data (0x01)
+        self.send_data(0x01)
 	self.send_command(0x10)
 
 #        image1 = [0xFF] * int(self.width * self.height / 8)
