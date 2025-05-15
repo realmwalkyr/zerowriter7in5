fa15109d0c6a424d9ed5c4c23c65f98ceaf238d0
realmwalkyr <88550926+realmwalkyr@users.noreply.github.com>
2025-05-15 10:32:02 -0500

    Update epd7in5_V2.py

--- e-Paper/RaspberryPi_JetsonNano/python/examples/waveshare_epd/epd7in5_V2.py
+++ e-Paper/RaspberryPi_JetsonNano/python/examples/waveshare_epd/epd7in5_V2.py
@@ -339,9 +339,9 @@ class EPD:
 #        Width = (Xend - Xstart) // 8
 #        Height = Yend - Ystart
 	
-        #self.send_command(0x50)
-        #self.send_data(0xA9)
-        #self.send_data(0x07)
+        self.send_command(0x50)
+        self.send_data(0xA9)
+        self.send_data(0x07)
 
         self.send_command(0x92)		#This command makes the display enter partial mode
         self.send_command(0x90)		#resolution setting
@@ -368,7 +368,7 @@ class EPD:
         self.send_data2(image)
 
         self.send_command(0x12)
-        #epdconfig.delay_ms(100)
+        epdconfig.delay_ms(10)
         #self.ReadBusy()
 
     def display_4Gray(self, image):
